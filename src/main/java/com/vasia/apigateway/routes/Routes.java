9ffaef9d167f563a8f63e7d3a8f67aa370ddf084
allyRx <herindrainyall2003@gmail.com>
2026-02-09 22:43:27 +0300

    init

---
+++ src/main/java/com/vasia/apigateway/routes/Routes.java
@@ -0,0 +1,14 @@
+package com.vasia.apigateway.routes;
+
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
+
+
+@Configuration
+public class Routes {
+
+
+
+}
