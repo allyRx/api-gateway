52ef4d52e2e509350b41ed6b4c4caaa1d00a3188
allyRx <herindrainyall2003@gmail.com>
2026-02-10 00:31:31 +0300

    add get product service

--- src/main/java/com/vasia/apigateway/routes/Routes.java
+++ src/main/java/com/vasia/apigateway/routes/Routes.java
@@ -1,14 +1,22 @@
 package com.vasia.apigateway.routes;
 
-
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.*;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @Configuration
 public class Routes {
 
-
+    @Bean
+    public RouterFunction<ServerResponse> productRoutes(){
+        return route("product_service")
+                .GET("/api/product", http()).before(uri("http://localhost:5050"))
+                .build();
+    }
 
 }
